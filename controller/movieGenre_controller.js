603576c274df4b4483c31771eb567ba51caa7a7a
Maulanaaryo <Maulanaaryo151@gmail.com>
2023-09-28 17:39:35 +0700

    menambahkan movieGenres untuk mengatur movieId dan genreId

--- controller/movieGenre_controller.js
+++ controller/movieGenre_controller.js
@@ -7,24 +7,65 @@ class MovieGenreController {
         order: [["id", "asc"]],
         include: [movie, genre],
       });
-      res.json(result);
+      res.render("movie_genre.ejs", { MG: result });
     } catch (e) {
       res.json(e);
     }
   }
 
-  static addPage(req, res) {}
+  static addPage(req, res) {
+    res.render("movie_genre_add.ejs");
+  }
 
   static async addMovieGenre(req, res) {
     try {
       const { movieId, genreId } = req.body;
+
+      const findMovie = await movie.findByPk(movieId);
+      if (!findMovie) {
+        throw {
+          message: `Movie id not found`,
+        };
+      }
+
+      const findGenre = await genre.findByPk(genreId);
+      if (!findGenre) {
+        throw {
+          message: `Genre id not found`,
+        };
+      }
 
       let result = await movieGenre.create({
         movieId: +movieId,
         genreId: +genreId,
       });
 
-      res.json(result);
+      res.redirect("/movieGenres");
+      // res.json(result);
+    } catch (e) {
+      res.json(e);
+    }
+  }
+
+  // static async updatePage(req, res) {}
+
+  // static async update(req, res) {}
+
+  static async delete(req, res) {
+    try {
+      const id = +req.params.id;
+
+      let result = await movieGenre.destroy({
+        where: { id },
+      });
+
+      result === 1
+        ? res.json({
+            message: `ID ${id} has been deleted`,
+          })
+        : res.json({
+            message: `ID ${id} not found`,
+          });
     } catch (e) {
       res.json(e);
     }
--- routes/movieGenre.js
+++ routes/movieGenre.js
@@ -6,6 +6,6 @@ movieGenreRoute.get("/add", movieGenreController.addPage);
 movieGenreRoute.post("/add", movieGenreController.addMovieGenre);
 // movieGenreRoute.get("/update/:id", movieGenreController.updatePage);
 // movieGenreRoute.post("/update/:id", movieGenreController.update);
-// movieGenreRoute.get("/remove/:id", movieGenreController.delete);
+movieGenreRoute.get("/remove/:id", movieGenreController.delete);
 
 module.exports = movieGenreRoute;
